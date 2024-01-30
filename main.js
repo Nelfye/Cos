19648186ad1517a5fa2846752adfce88b5ed50ab
Nelfye <147432084+Nelfye@users.noreply.github.com>
2024-01-30 12:35:23 -0500

    Update main.js to test

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const ytdl = require('ytdl-core');
 const ytsr = require('ytsr');
 const client = new Discord.Client({ intents: 3276799 });
 const config = require("./config.json");
-const { title } = require("process");
 
 let isPlaying = false;
 let isLooping = false;
@@ -16,27 +15,35 @@ client.login(config.token);
 client.on("ready", async () => {
     await client.application.commands.set(commandsList);
 
-    console.log(`🟢${client.user.tag} V2`);
+    console.log(`🟢${client.user.tag} / be Nelfye` + config.npm + config.serveurLink);
 
     commandsList.forEach(command => {
         console.log(`Commande enregistrée : ${command.name}`);
     });
-
-    setInterval(() => {
         client.user.setPresence({
             activities: [{
-                name: title,
+                name: "online",
                 type: Discord.ActivityType.Listening
             }],
-            status: "idle"
+            status: "dnd"
         });
-    }, 1000);
 });
 
 const commandsList = [
-    
     {
-        name: "search",
+        name: "help",
+        description: "Affiche la liste des commandes",
+        handle: (interaction) => {
+            const commandsDescription = commandsList.map(command => `**${command.name}**: ${command.description}`).join("\n");
+            const embed = {
+                title: "Liste des commandes",
+                description: commandsDescription
+            };
+            interaction.reply({ content: "", embeds: [embed], ephemeral: true });
+        },
+    },
+    {
+        name: "play",
         description: "Recherche et joue une musique par titre",
         handle: async (interaction) => {
             const titre = interaction.options.getString("titre");
@@ -64,7 +71,13 @@ const commandsList = [
             const url = firstResult.url;
             const videoID = firstResult.id;
             const title = firstResult.title;
-
+            client.user.setPresence({
+                activities: [{
+                    name: title,
+                    type: Discord.ActivityType.Listening
+                }],
+                status: "idle"
+            });
             const stream = ytdl(url, { filter: 'audioonly' });
             const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
 
@@ -92,19 +105,6 @@ const commandsList = [
                 required: true,
             },
         ],
-    },
-    
-    {
-        name: "help",
-        description: "Affiche la liste des commandes",
-        handle: (interaction) => {
-            const commandsDescription = commandsList.map(command => `**${command.name}**: ${command.description}`).join("\n");
-            const embed = {
-                title: "Liste des commandes",
-                description: commandsDescription
-            };
-            interaction.reply({ content: "", embeds: [embed], ephemeral: true });
-        },
     },
     {
         name: "play",
@@ -154,130 +154,20 @@ const commandsList = [
             },
         ],
     },
-    {
-        name: "playliste",
-        description: "Joue une liste de musique avec des liens",
-        handle: async (interaction) => {
-            const urls = interaction.options.getString("urls");
-            if (!urls) {
-                return interaction.reply("Veuillez fournir une liste d'URLs YouTube valides séparées par des virgules.");
-            }
     
-            const urlArray = urls.split(",").map(url => url.trim());
-    
-            if (urlArray.length === 0) {
-                return interaction.reply("Veuillez fournir une liste d'URLs YouTube valides séparées par des virgules.");
-            }
-    
-            if (isPlaying) {
-                playlist.push(...urlArray.map(url => ({ url, videoID: ytdl.getURLVideoID(url) })));
-                interaction.reply(`Musiques ajoutées à la playlist : ${urlArray.join(", ")}`);
-            } else {
-                const channel = interaction.member.voice.channel;
-                if (!channel) {
-                    return interaction.reply("Vous devez être dans un salon vocal pour utiliser cette commande.");
-                }
-    
-                const connection = joinVoiceChannel({
-                    channelId: channel.id,
-                    guildId: channel.guild.id,
-                    adapterCreator: channel.guild.voiceAdapterCreator,
-                });
-    
-                playlist.push(...urlArray.map(url => ({ url, videoID: ytdl.getURLVideoID(url) })));
-                isPlaying = true;
-                playCurrentSong(connection);
-                
-                const embeds = await Promise.all(urlArray.map(async (url) => {
-                    const videoID = ytdl.getURLVideoID(url);
-                    const title = await ytdl.getBasicInfo(url).then(info => info.videoDetails.title);
-                    const videoimage = `https://img.youtube.com/vi/${videoID}/maxresdefault.jpg`;
-    
-                    return {
-                        title: `Musique ajoutée à la playlist`,
-                        description: `**Titre**: ${title}\n**Lien**: ${url}\n`,
-                        image: {
-                            url: videoimage,
-                        },
-                    };
-                }));
-    
-                interaction.reply({ content: "", embeds: embeds, ephemeral: false });
-            }
-        },
-        options: [
-            {
-                name: "urls",
-                description: "Liste d'URLs YouTube séparées par des virgules",
-                type: 3,
-                required: true,
-            },
-        ],
-    },
     {
     name: "skip",
     description: "Passe à la musique suivante dans la playlist",
     handle: async (interaction) => {
-        const channel = interaction.member.voice.channel;
-        if (!channel) {
-            return interaction.reply("Vous devez être dans un salon vocal pour utiliser cette commande.");
-        }
 
-        if (playlist.length === 0) {
-            return interaction.reply("Il n'y a pas de musique suivante dans la playlist.");
-        }
-
-        const nextSong = playlist[0];
-        const { url, videoID } = nextSong;
-
-        const connection = joinVoiceChannel({
-            channelId: channel.id,
-            guildId: channel.guild.id,
-            adapterCreator: channel.guild.voiceAdapterCreator,
-        });
-
-        const player = createAudioPlayer();
-        connection.subscribe(player);
-        player.stop();
-
-        const stream = ytdl(url, { filter: 'audioonly' });
-        const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
-        player.play(resource);
-        playlist.shift();
-
-        const title = await ytdl.getBasicInfo(url).then(info => info.videoDetails.title);
-        const videoimage = `https://img.youtube.com/vi/${videoID}/maxresdefault.jpg`;
-
-        const embed = {
-            title: "Musique suivante",
-            description: `**Titre**: ${title}\n**Lien**: ${url}\n`,
-            image: {
-                url: videoimage,
-            },
-        };
-
-        interaction.reply({ content: "", embeds: [embed], ephemeral: false });
     },
 },
 
     {
         name: "stop",
-        description: "Arrête la lecture de musique et vide la playlist",
+        description: "Arrête la lecture de musique",
         handle: (interaction) => {
-            const channel = interaction.member.voice.channel;
-            if (!channel) {
-                return interaction.reply("Vous devez être dans un salon vocal pour utiliser cette commande.");
-            }
 
-            playlist.length = 0;
-            isPlaying = false;
-            const connection = joinVoiceChannel({
-                channelId: channel.id,
-                guildId: channel.guild.id,
-                adapterCreator: channel.guild.voiceAdapterCreator,
-            });
-            connection.destroy();
-            interaction.reply("La lecture de musique a été arrêtée et la playlist a été vidée.");
         },
     },
 ];
@@ -289,49 +179,4 @@ client.on("interactionCreate", (interaction) => {
     if (command) {
         command.handle(interaction);
     }
-});
-
-async function playCurrentSong(connection) {
-    if (playlist.length === 0) {
-        isPlaying = false;
-        if (isLooping) {
-            playlist.push(...loopPlaylist);
-        }
-        return;
-    }
-
-    const { url, videoID } = playlist[0];
-
-    const stream = ytdl(url, { filter: 'audioonly' });
-    const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
-
-    const player = createAudioPlayer();
-    player.play(resource);
-    connection.subscribe(player);
-
-    player.once('finish', () => {
-        playlist.shift();
-        playCurrentSong(connection);
-    });
-}
-
-client.on("voiceChannelJoin", (member, channel) => {
-    if (!isPlaying && playlist.length > 0) {
-        isPlaying = true;
-        const connection = joinVoiceChannel({
-            channelId: channel.id,
-            guildId: channel.guild.id,
-            adapterCreator: channel.guild.voiceAdapterCreator,
-        });
-
-        playCurrentSong(connection);
-    }
-});
-
-client.on("error", (error) => {
-    console.error('Erreur du client Discord :', error);
-});
-
-client.on("warn", (warning) => {
-    console.warn('Avertissement du client Discord :', warning);
 });
